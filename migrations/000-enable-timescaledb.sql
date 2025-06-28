528a55dc24ced7e6970537d4f2646a39b9dd415e
Ricardo Reyes <reyesramirezricardoemanuel@gmail.com>
2025-06-28 17:41:02 -0500

    refactor sql migrations

--- migrations/000-enable-timescaledb.sql
+++
@@ -1,6 +0,0 @@
--- 000-enable-timescaledb.sql
--- This migration ensures the TimescaleDB extension is enabled in the database.
--- It must run before any other migration that uses TimescaleDB functions.
--- depends:
-SELECT version();
-CREATE EXTENSION IF NOT EXISTS timescaledb;
