57a7db84fa70840559603c76dc58dddc3b2d6e97
Ricardo Reyes <reyesramirezricardoemanuel@gmail.com>
2025-07-02 23:55:21 -0500

    chore: clea unnecesary files

--- etl/pipelines/__init__.py
+++ etl/pipelines/__init__.py
@@ -8,12 +8,3 @@ Disponibles:
 - HybridRawDataPipeline: Pipeline híbrido calendario + watermarks
 - CampaignCatchUpPipeline: Pipeline de catch-up para campañas
 """
-
-from .raw_data_pipeline import HybridRawDataPipeline, ExtractionStrategy
-from .campaign_catchup_pipeline import CampaignCatchUpPipeline
-
-__all__ = [
-    'HybridRawDataPipeline',
-    'ExtractionStrategy',
-    'CampaignCatchUpPipeline'
-]
